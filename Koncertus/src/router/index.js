1421b60fa27ca374fd9d3750d8bf32b11ec80d93
Davor Kuharić <davorkuharic1997@gmail.com>
2023-02-05 00:33:20 +0100

    Start of developing
    adding files, starting with static from zero to hero

--- Koncertus/src/router/index.js
+++ Koncertus/src/router/index.js
@@ -1,5 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import HomePage from "@/views/HomePage.vue";
+import AboutPage from "@/views/AboutPage.vue";
+import ConcertsPage from "@/views/ConcertsPage.vue";
+import LoginPage from "@/views/LoginPage.vue";
+import RegisterPage from "@/views/RegisterPage.vue";
+import ProfilePage from "@/views/ProfilePage.vue";
+import ReservationsPage from "@/views/ReservationsPage.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,15 +13,37 @@ const router = createRouter({
     {
       path: "/",
       name: "home",
-      component: HomeView,
+      component: HomePage
     },
     {
       path: "/about",
       name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import("../views/AboutView.vue"),
+      component: AboutPage
+    },
+    {
+      path: "/concerts",
+      name: "concerts",
+      component: ConcertsPage
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: LoginPage
+    },
+    {
+      path: "/register",
+      name: "register",
+      component: RegisterPage
+    },
+    {
+      path: "/profile",
+      name: "profile",
+      component: ProfilePage
+    },
+    {
+      path: "/reservations",
+      name: "reservations",
+      component: ReservationsPage
     },
   ],
 });
---
+++ Koncertus/src/views/ConcertsPage.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Concerts</h1>
+</template>
