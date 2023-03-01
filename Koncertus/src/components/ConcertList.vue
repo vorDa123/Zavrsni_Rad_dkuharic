11348d72bacfe2fb9022e41d588b2ccbe7a17ca0
Davor Kuharić <davorkuharic1997@gmail.com>
2023-03-01 18:44:07 +0100

    Concert list section started

--- Koncertus/src/components/ConcertList.vue
+++ Koncertus/src/components/ConcertList.vue
@@ -1,6 +1,23 @@
 <template>
   <div class="wrapper marginTop">
     <h2 class="uppercase">Upcoming concerts</h2>
-    <div class=""></div>
+    <div class="listOfConcerts">
+      <div class="row">
+        <div class="column">
+          <div class="odd left">Some Text in Column One</div>
+        </div>
+        <div class="column">
+          <div class="odd right">Some Text in Column Two</div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="column">
+          <div class="even left">Some Text in Column One</div>
+        </div>
+        <div class="column">
+          <div class="even right">Some Text in Column Two</div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
