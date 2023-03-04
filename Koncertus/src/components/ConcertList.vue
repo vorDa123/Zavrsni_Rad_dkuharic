c05d7e6236b72a68d49d0a12b5de6861b5b861ee
Davor Kuharić <davorkuharic1997@gmail.com>
2023-03-04 15:21:54 +0100

    concert list in concert page finished

--- Koncertus/src/components/ConcertList.vue
+++ Koncertus/src/components/ConcertList.vue
@@ -4,18 +4,174 @@
     <div class="listOfConcerts">
       <div class="row">
         <div class="column">
-          <div class="odd left">Some Text in Column One</div>
+          <div class="odd left inline">
+            <div class="paddingIcons paddingTopDate">
+              <p class="bold">16</p>
+              <p class="bold">NOV</p>
+            </div>
+            <div class="inline paddingIcons paddingTopContent widthContent">
+              <div class="concertImage" />
+              <div class="paddingIcons">
+                <p class="bold">DISTURBED</p>
+                <p class="descriptionFontSize">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+                  sed enim eu leo dignissim elementum ut a urna.
+                </p>
+                <div class="inline iconFontSize">
+                  <p>
+                    <font-awesome-icon icon="fa-solid fa-calendar" />
+                    16.11.2023.
+                  </p>
+                  <p>
+                    <font-awesome-icon
+                      icon="fa-solid fa-clock"
+                      class="paddingIcons"
+                    />21:45
+                  </p>
+                  <p>
+                    <font-awesome-icon
+                      icon="fa-solid fa-location-dot"
+                      class="paddingIcons"
+                    />Arena Zagreb
+                  </p>
+                  <p class="paddingIcons">Price: 100 EUR</p>
+                </div>
+              </div>
+              <div class="alignCenter shoppingCart">
+                <font-awesome-icon icon="fa-solid fa-cart-shopping" />
+                <br />
+                <button class="buttonMargin whiteButton">BUY</button>
+              </div>
+            </div>
+          </div>
         </div>
         <div class="column">
-          <div class="odd right">Some Text in Column Two</div>
+          <div class="odd right inline">
+            <div class="paddingIcons paddingTopDate">
+              <p class="bold">16</p>
+              <p class="bold">NOV</p>
+            </div>
+            <div class="inline paddingIcons paddingTopContent widthContent">
+              <div class="concertImage" />
+              <div class="paddingIcons">
+                <p class="bold">DISTURBED</p>
+                <p class="descriptionFontSize">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+                  sed enim eu leo dignissim elementum ut a urna.
+                </p>
+                <div class="inline iconFontSize">
+                  <p>
+                    <font-awesome-icon icon="fa-solid fa-calendar" />
+                    16.11.2023.
+                  </p>
+                  <p>
+                    <font-awesome-icon
+                      icon="fa-solid fa-clock"
+                      class="paddingIcons"
+                    />21:45
+                  </p>
+                  <p>
+                    <font-awesome-icon
+                      icon="fa-solid fa-location-dot"
+                      class="paddingIcons"
+                    />Arena Zagreb
+                  </p>
+                  <p class="paddingIcons">Price: 100 EUR</p>
+                </div>
+              </div>
+              <div class="alignCenter shoppingCart">
+                <font-awesome-icon icon="fa-solid fa-cart-shopping" />
+                <br />
+                <button class="buttonMargin whiteButton">BUY</button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
       <div class="row">
         <div class="column">
-          <div class="even left">Some Text in Column One</div>
+          <div class="even left inline">
+            <div class="paddingIcons paddingTopDate">
+              <p class="bold">16</p>
+              <p class="bold">NOV</p>
+            </div>
+            <div class="inline paddingIcons paddingTopContent widthContent">
+              <div class="concertImage" />
+              <div class="paddingIcons">
+                <p class="bold">DISTURBED</p>
+                <p class="descriptionFontSize">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+                  sed enim eu leo dignissim elementum ut a urna.
+                </p>
+                <div class="inline iconFontSize">
+                  <p>
+                    <font-awesome-icon icon="fa-solid fa-calendar" />
+                    16.11.2023.
+                  </p>
+                  <p>
+                    <font-awesome-icon
+                      icon="fa-solid fa-clock"
+                      class="paddingIcons"
+                    />21:45
+                  </p>
+                  <p>
+                    <font-awesome-icon
+                      icon="fa-solid fa-location-dot"
+                      class="paddingIcons"
+                    />Arena Zagreb
+                  </p>
+                  <p class="paddingIcons">Price: 100 EUR</p>
+                </div>
+              </div>
+              <div class="alignCenter shoppingCart">
+                <font-awesome-icon icon="fa-solid fa-cart-shopping" />
+                <br />
+                <button class="buttonMargin blueButton">BUY</button>
+              </div>
+            </div>
+          </div>
         </div>
         <div class="column">
-          <div class="even right">Some Text in Column Two</div>
+          <div class="even right inline">
+            <div class="paddingIcons paddingTopDate">
+              <p class="bold">16</p>
+              <p class="bold">NOV</p>
+            </div>
+            <div class="inline paddingIcons paddingTopContent widthContent">
+              <div class="concertImage" />
+              <div class="paddingIcons">
+                <p class="bold">DISTURBED</p>
+                <p class="descriptionFontSize">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+                  sed enim eu leo dignissim elementum ut a urna.
+                </p>
+                <div class="inline iconFontSize">
+                  <p>
+                    <font-awesome-icon icon="fa-solid fa-calendar" />
+                    16.11.2023.
+                  </p>
+                  <p>
+                    <font-awesome-icon
+                      icon="fa-solid fa-clock"
+                      class="paddingIcons"
+                    />21:45
+                  </p>
+                  <p>
+                    <font-awesome-icon
+                      icon="fa-solid fa-location-dot"
+                      class="paddingIcons"
+                    />Arena Zagreb
+                  </p>
+                  <p class="paddingIcons">Price: 100 EUR</p>
+                </div>
+              </div>
+              <div class="alignCenter shoppingCart">
+                <font-awesome-icon icon="fa-solid fa-cart-shopping" />
+                <br />
+                <button class="buttonMargin blueButton">BUY</button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
