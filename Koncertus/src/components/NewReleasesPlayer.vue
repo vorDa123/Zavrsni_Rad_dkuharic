d79e08d2ea6890d4ee6f3c3a51e0727dcba03ae9
Davor Kuharić <davorkuharic1997@gmail.com>
2023-03-05 01:46:38 +0100

    Concertlist and top10 section

---
+++ Koncertus/src/components/NewReleasesPlayer.vue
@@ -0,0 +1,269 @@
+<template>
+  <div class="row">
+    <div class="column">
+      <div class="topTenSongs">
+        <br />
+        <p class="uppercase bold positionTopTenTitle">Top ten songs</p>
+        <div class="row rowGap">
+          <div class="column">
+            <div class="inline marginLeftNumber">
+              <div class="paddingIcons paddingTopNumber">
+                <p class="bold">#1</p>
+              </div>
+              <div class="inline">
+                <div class="songImage" />
+                <div class="paddingIcons lineHeight">
+                  <p class="bold">DISTURBED</p>
+                  <p class="smallerFont">Uninvited guest</p>
+                  <p class="smallerFont">Album1</p>
+                  <div class="sameLine paddingSameLine">
+                    <p class="smallerFont">4:15</p>
+                    <button class="playButtonStyle">
+                      <font-awesome-icon
+                        icon="fa-solid fa-play"
+                        class="paddingRightCardIcon"
+                      />Play
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="column">
+            <div class="inline marginLeftNumberSecondColumn">
+              <div class="paddingIcons paddingTopNumber">
+                <p class="bold">#1</p>
+              </div>
+              <div class="inline">
+                <div class="songImage" />
+                <div class="paddingIcons lineHeight">
+                  <p class="bold">DISTURBED</p>
+                  <p class="smallerFont">Uninvited guest</p>
+                  <p class="smallerFont">Album1</p>
+                  <div class="sameLine paddingSameLine">
+                    <p class="smallerFont">4:15</p>
+                    <button class="playButtonStyle">
+                      <font-awesome-icon
+                        icon="fa-solid fa-play"
+                        class="paddingRightCardIcon"
+                      />Play
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row rowGap">
+          <div class="column">
+            <div class="inline marginLeftNumber">
+              <div class="paddingIcons paddingTopNumber">
+                <p class="bold">#1</p>
+              </div>
+              <div class="inline">
+                <div class="songImage" />
+                <div class="paddingIcons lineHeight">
+                  <p class="bold">DISTURBED</p>
+                  <p class="smallerFont">Uninvited guest</p>
+                  <p class="smallerFont">Album1</p>
+                  <div class="sameLine paddingSameLine">
+                    <p class="smallerFont">4:15</p>
+                    <button class="playButtonStyle">
+                      <font-awesome-icon
+                        icon="fa-solid fa-play"
+                        class="paddingRightCardIcon"
+                      />Play
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="column">
+            <div class="inline marginLeftNumberSecondColumn">
+              <div class="paddingIcons paddingTopNumber">
+                <p class="bold">#1</p>
+              </div>
+              <div class="inline">
+                <div class="songImage" />
+                <div class="paddingIcons lineHeight">
+                  <p class="bold">DISTURBED</p>
+                  <p class="smallerFont">Uninvited guest</p>
+                  <p class="smallerFont">Album1</p>
+                  <div class="sameLine paddingSameLine">
+                    <p class="smallerFont">4:15</p>
+                    <button class="playButtonStyle">
+                      <font-awesome-icon
+                        icon="fa-solid fa-play"
+                        class="paddingRightCardIcon"
+                      />Play
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row rowGap">
+          <div class="column">
+            <div class="inline marginLeftNumber">
+              <div class="paddingIcons paddingTopNumber">
+                <p class="bold">#1</p>
+              </div>
+              <div class="inline">
+                <div class="songImage" />
+                <div class="paddingIcons lineHeight">
+                  <p class="bold">DISTURBED</p>
+                  <p class="smallerFont">Uninvited guest</p>
+                  <p class="smallerFont">Album1</p>
+                  <div class="sameLine paddingSameLine">
+                    <p class="smallerFont">4:15</p>
+                    <button class="playButtonStyle">
+                      <font-awesome-icon
+                        icon="fa-solid fa-play"
+                        class="paddingRightCardIcon"
+                      />Play
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="column">
+            <div class="inline marginLeftNumberSecondColumn">
+              <div class="paddingIcons paddingTopNumber">
+                <p class="bold">#1</p>
+              </div>
+              <div class="inline">
+                <div class="songImage" />
+                <div class="paddingIcons lineHeight">
+                  <p class="bold">DISTURBED</p>
+                  <p class="smallerFont">Uninvited guest</p>
+                  <p class="smallerFont">Album1</p>
+                  <div class="sameLine paddingSameLine">
+                    <p class="smallerFont">4:15</p>
+                    <button class="playButtonStyle">
+                      <font-awesome-icon
+                        icon="fa-solid fa-play"
+                        class="paddingRightCardIcon"
+                      />Play
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row rowGap">
+          <div class="column">
+            <div class="inline marginLeftNumber">
+              <div class="paddingIcons paddingTopNumber">
+                <p class="bold">#1</p>
+              </div>
+              <div class="inline">
+                <div class="songImage" />
+                <div class="paddingIcons lineHeight">
+                  <p class="bold">DISTURBED</p>
+                  <p class="smallerFont">Uninvited guest</p>
+                  <p class="smallerFont">Album1</p>
+                  <div class="sameLine paddingSameLine">
+                    <p class="smallerFont">4:15</p>
+                    <button class="playButtonStyle">
+                      <font-awesome-icon
+                        icon="fa-solid fa-play"
+                        class="paddingRightCardIcon"
+                      />Play
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="column">
+            <div class="inline marginLeftNumberSecondColumn">
+              <div class="paddingIcons paddingTopNumber">
+                <p class="bold">#1</p>
+              </div>
+              <div class="inline">
+                <div class="songImage" />
+                <div class="paddingIcons lineHeight">
+                  <p class="bold">DISTURBED</p>
+                  <p class="smallerFont">Uninvited guest</p>
+                  <p class="smallerFont">Album1</p>
+                  <div class="sameLine paddingSameLine">
+                    <p class="smallerFont">4:15</p>
+                    <button class="playButtonStyle">
+                      <font-awesome-icon
+                        icon="fa-solid fa-play"
+                        class="paddingRightCardIcon"
+                      />Play
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row rowGap">
+          <div class="column">
+            <div class="inline marginLeftNumber">
+              <div class="paddingIcons paddingTopNumber">
+                <p class="bold">#1</p>
+              </div>
+              <div class="inline">
+                <div class="songImage" />
+                <div class="paddingIcons lineHeight">
+                  <p class="bold">DISTURBED</p>
+                  <p class="smallerFont">Uninvited guest</p>
+                  <p class="smallerFont">Album1</p>
+                  <div class="sameLine paddingSameLine">
+                    <p class="smallerFont">4:15</p>
+                    <button class="playButtonStyle">
+                      <font-awesome-icon
+                        icon="fa-solid fa-play"
+                        class="paddingRightCardIcon"
+                      />Play
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="column">
+            <div class="inline marginLeftNumberSecondColumn">
+              <div class="paddingIcons paddingTopNumber">
+                <p class="bold">#1</p>
+              </div>
+              <div class="inline">
+                <div class="songImage" />
+                <div class="paddingIcons lineHeight">
+                  <p class="bold">DISTURBED</p>
+                  <p class="smallerFont">Uninvited guest</p>
+                  <p class="smallerFont">Album1</p>
+                  <div class="sameLine paddingSameLine">
+                    <p class="smallerFont">4:15</p>
+                    <button class="playButtonStyle">
+                      <font-awesome-icon
+                        icon="fa-solid fa-play"
+                        class="paddingRightCardIcon"
+                      />Play
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <br />
+      </div>
+    </div>
+    <div class="column">
+      <div class="backgroundImagePlayer">
+        <div class="nowPlaying">
+          <div class="playerContent"></div>
+        </div>
+      </div>
+      <div class="playNext">Play next</div>
+    </div>
+  </div>
+</template>
--- Koncertus/src/views/ConcertsPage.vue
+++ Koncertus/src/views/ConcertsPage.vue
@@ -1,15 +1,18 @@
 <template>
   <ConcertList/>
+  <NewReleasesPlayer/>
   <Footer />
 </template>
 <script>
 import Footer from "@/components/Footer.vue";
 import ConcertList from "@/components/ConcertList.vue";
+import NewReleasesPlayer from "@/components/NewReleasesPlayer.vue";
 
 export default {
   components: {
     Footer,
     ConcertList,
+    NewReleasesPlayer,
   },
 }
 </script>
