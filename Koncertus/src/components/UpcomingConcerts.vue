cbdd57ebb58402a3657f6f14dfb2b6eec6adc006
Davor Kuharić <davorkuharic1997@gmail.com>
2023-03-19 16:18:00 +0100

    changes in style, trying responsiveness

--- Koncertus/src/components/UpcomingConcerts.vue
+++ Koncertus/src/components/UpcomingConcerts.vue
@@ -16,7 +16,7 @@
                     <div class="sameLine">
                         <p><font-awesome-icon icon="fa-solid fa-location-dot" class="paddingRightCardIcon" /> Arena
                             Zagreb</p>
-                        <p class="alignRight">20 EUR</p>
+                        <p class="alignRight bold">20 EUR</p>
                     </div>
                 </div>
             </div>
@@ -29,7 +29,7 @@
                     <div class="sameLine">
                         <p><font-awesome-icon icon="fa-solid fa-location-dot" class="paddingRightCardIcon" /> Arena
                             Zagreb</p>
-                        <p class="alignRight">20 EUR</p>
+                        <p class="alignRight bold">20 EUR</p>
                     </div>
                 </div>
             </div>
@@ -42,7 +42,7 @@
                     <div class="sameLine">
                         <p><font-awesome-icon icon="fa-solid fa-location-dot" class="paddingRightCardIcon" /> Arena
                             Zagreb</p>
-                        <p class="alignRight">20 EUR</p>
+                        <p class="alignRight bold">20 EUR</p>
                     </div>
                 </div>
             </div>
@@ -55,7 +55,20 @@
                     <div class="sameLine">
                         <p><font-awesome-icon icon="fa-solid fa-location-dot" class="paddingRightCardIcon" /> Arena
                             Zagreb</p>
-                        <p>20 EUR</p>
+                        <p class="alignRight bold">20 EUR</p>
+                    </div>
+                </div>
+            </div>            
+            <div class="concertCard">
+                <div class="concertCardImage"></div>
+                <div class="concertCardText">
+                    <p class="concertCardTextTitle uppercase">Disturbed</p>
+                    <p><font-awesome-icon icon="fa-solid fa-calendar" class="paddingRightCardIcon" /> 05.02.2023.</p>
+                    <p><font-awesome-icon icon="fa-solid fa-clock" class="paddingRightCardIcon" /> 20:15</p>
+                    <div class="sameLine">
+                        <p><font-awesome-icon icon="fa-solid fa-location-dot" class="paddingRightCardIcon" /> Arena
+                            Zagreb</p>
+                        <p class="alignRight bold">20 EUR</p>
                     </div>
                 </div>
             </div>
